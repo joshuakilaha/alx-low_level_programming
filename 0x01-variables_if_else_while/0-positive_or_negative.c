292287f224e2b76fcf86bd57d7268422a9c89d11
joshua kilaha <jokilaha191@gmail.com>
2022-03-12 00:14:47 +0300

    fixed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,6 +13,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0) 
 	{
 		printf("%i is positive\n",n);
@@ -25,5 +26,6 @@ int main(void)
 	{
 		printf("%i is zero\n", n);
 	}
+
 	return (0);
 }
