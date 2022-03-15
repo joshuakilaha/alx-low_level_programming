f9b3d4429fdea016602bb9745c94197ae4c5bfae
joshua kilaha <jokilaha191@gmail.com>
2022-03-16 00:17:44 +0300

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - function for lowercase
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet()
+{
+	char alp;
+	
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		_putchar(alp);
+	}
+	_putchar('\n');
+
+}
