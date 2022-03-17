39f4d36bdacca665c409a06606f1e2396eb32ddb
joshua kilaha <jokilaha191@gmail.com>
2022-03-18 00:23:03 +0300

    10 times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* more_numbers - function prints (0 to 14) 10 times
+*/
+
+void more_numbers(void)
+{
+	char i;
+	int count;
+
+	for (count = 1; count <= 10; count++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			if (i / 10 > 0)
+				_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
