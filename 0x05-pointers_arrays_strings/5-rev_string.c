c4cbd601e3bd272b1ec80514037704e27c66b1ce
joshua kilaha <jokilaha191@gmail.com>
2022-03-22 22:10:02 +0300

    reverse

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,24 +7,24 @@
 
 void rev_string(char *s)
 {
-    char t;
-    int i, length, length2;
+	char t;
+	int i, length, length2;
 
-    length = 0;
-    length2 = 0;
+	length = 0;
+	length2 = 0;
 
-    while (s[length] != '\0')
-    {
-        length++;
-    }
+	while (s[length] != '\0')
+	{
+	length++;
+	}
     
-    length2 = length - 1;
+	length2 = length - 1;
 
-    for (i = 0; i < length / 2; i++)
-    {
-        t = s[i];
-        s[i] = s[length2];
-        s[length2--] = t;
+	for (i = 0; i < length / 2; i++)
+	{
+	t = s[i];
+	s[i] = s[length2];
+	s[length2--] = t;
         
-    }
+	}
 }
