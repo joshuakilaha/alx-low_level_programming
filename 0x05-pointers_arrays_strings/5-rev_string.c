485c81b09c0f4119322ce55d8b3308d92339454e
joshua kilaha <jokilaha191@gmail.com>
2022-03-22 22:06:01 +0300

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* rev_string - function thar reverses a string
+* @s: character to be checked
+*/
+
+void rev_string(char *s)
+{
+    char t;
+    int i, length, length2;
+
+    length = 0;
+    length2 = 0;
+
+    while (s[length] != '\0')
+    {
+        length++;
+    }
+    
+    length2 = length - 1;
+
+    for (i = 0; i < length / 2; i++)
+    {
+        t = s[i];
+        s[i] = s[length2];
+        s[length2--] = t;
+        
+    }
+}
