9c2c59fa5bd693ca3463477c39f30399fa299fe1
joshua kilaha <jokilaha191@gmail.com>
2022-04-02 00:39:57 +0300

    add betty fix

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,31 +12,31 @@
 
 int main(int argc, char *argv[])
 {
-    int sum, num, i, j, k;
+	int sum, num, i, j, k;
 
-    sum = 0;
+	sum = 0;
 
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (argv[i][j] > '9' || argv[i][j] < '0')
-            {
-                puts("Error");
-                return (1);
-            }
-        }
-    }
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				printf("Error");
+				return (1);
+			}
+		}
+	}
 
-    for (k = 1; k < argc; k++)
-    {
-        num = atoi(argv[k]);
-        if (num >= 0)
-        {
-            sum += num;
-        }
-    }
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
 
-    printf("%d\n", sum);
-    return (0);
+	printf("%d\n", sum);
+	return (0);
 }
